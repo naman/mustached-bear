2646662fbccf78b1340b5af39cecf08c332ea73b
Naman <naman13064@iiitd.ac.in>
2015-05-02 16:33:23 +0530

    model

--- dbms/app/models.py
+++ dbms/app/models.py
@@ -1,4 +1,32 @@
 from django.db import models
 
 # Create your models here.
-class 
+class Employee(models.Model):
+        employee_no = models.IntgerField(primary_key=True)
+        fname = models.CharField(max_length=50)
+        lname = models.CharField(max_length = 50)
+        zip = models.IntegerField(max_length=6, null = True)
+
+class Customer(models.Model):
+    customer_no = models.IntgerField(primary_key=True)
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length = 50)
+    zip = models.IntegerField(max_length=6, null = True)
+
+class Part(models.Model):
+    part_no = models.IntgerField(primary_key=True)
+    pname = models.CharField(max_length = 20)
+    price = models.IntgerField(max_length=4)
+    max_quantity = models.IntegerField(max_length=4) #This is the stock quantity for every part
+
+class Order(models.Model):
+    order_no = models.IntgerField(primary_key = True)
+    employee = models.ForeignKey(Employee)
+    customer = models.ForeignKey(Customer)
+    recv_date = models.DateTimeField()
+    ship_date = models.DateTimeField()
+
+class Order_Part_Quantity(models.Model):
+    order_no = models.ForeignKey(Order)
+    part_quantity = models.IntgerField()
+    part_no = models.ForeignKey(Part)
